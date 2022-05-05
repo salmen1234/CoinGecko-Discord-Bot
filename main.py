336403131d24188d762ab23698926d1f02b38831
salmen1234 <105023745+salmen1234@users.noreply.github.com>
2022-05-06 01:19:05 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+import requests
+from bs4 import BeautifulSoup
+
+bot = commands.Bot(command_prefix='?',description='Bot')
+
+token = 'OTcxODg3MjA1NjI0NzA5MTgw.YnRCfg.D-4fF2nkAOKtfbnhDrNpZrTtuhM'
+
+print('rédy')
+
+@bot.command()
+async def stats(ctx, value1, value2):
+    url = 'https://r6.tracker.network/profile/' + value1 + '/' + value2 
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    r_divs = soup.findAll('div', {'class': 'trn-text--dimmed'})
+    mmr_divs = soup.findAll('div', {'style': 'font-family: Rajdhani; font-size: 3rem;'})
+    ki_div = soup.findAll('div', {'data-stat':'RankedKills'})
+    w_divs = soup.findAll('div', {'data-stat':'RankedWins'})
+    wr_divs = soup.findAll('div', {'data-stat':'RankedWLRatio'})
+    kd_divs = soup.findAll('div', {'data-stat':'RankedKDRatio'})
+    ma_divs = soup.findAll('div', {'data-stat':'RankedMatches'})
+
+    Bmmr = mmr_divs[0]
+    rank = r_divs[0] 
+    kills = ki_div[0]
+    wins = w_divs[0]
+    winR = wr_divs[0]
+    kd = kd_divs[0]
+    matches = ma_divs[0]
+
+    embed = discord.Embed(title='Statistiques de : ' + value2, url=url, color=0x6CCF24)
+    embed.add_field(name='Rank 🏆',value=rank.text)
+    embed.add_field(name='Best MMR 🚀',value=Bmmr.text)
+    embed.add_field(name='Wins 👑',value=wins.text)
+    embed.add_field(name='Win % 👑',value=winR.text)
+    embed.add_field(name='Kills 🔫',value=kills.text)
+    embed.add_field(name='KD 🌍',value=kd.text)
+    embed.add_field(name='Matches 👥',value=matches.text)
+
+    await ctx.send(embed=embed)
+    
+bot.run(token)
